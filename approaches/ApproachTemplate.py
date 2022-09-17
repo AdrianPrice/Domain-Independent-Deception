5bff53641d71e6f3782c2c30c9f93c816954842d
Adrian Price <adrian.price@monash.edu>
2022-09-17 16:12:57 +1000

    added extra centroid approaches

--- approaches/ApproachTemplate.py
+++ approaches/ApproachTemplate.py
@@ -1,8 +1,9 @@
 class ApproachTemplate():
-    def __init__(self, extractedLandmarks, realTask, hashableRealGoal):
+    def __init__(self, extractedLandmarks, realTask, hashableRealGoal, dname):
         self.l = extractedLandmarks
         self.realTask = realTask
         self.hashableRealGoal = hashableRealGoal
+        self.dname = dname
 
     def generate(self):
         pass
--- approaches/BaselineApproach.py
+++ approaches/BaselineApproach.py
@@ -6,8 +6,8 @@ class BaselineApproach(ApproachTemplate):
     Calculates the optimal path from the initial state to the real goal.
     """
 
-    def __init__(self, extractedLandmarks, realTask, hashableRealGoal):
-        super().__init__(extractedLandmarks, realTask, hashableRealGoal)
+    def __init__(self, extractedLandmarks, realTask, hashableRealGoal, dname):
+        super().__init__(extractedLandmarks, realTask, hashableRealGoal, dname)
 
     def generate(self):
         ordered_l = []
