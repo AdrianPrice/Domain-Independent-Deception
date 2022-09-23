f1f387388ff20a88d7c88c931850b2cba2621848
Ramon Fraga Pereira <ramonfpereira@gmail.com>
2022-09-23 22:11:42 +0200

    Adding relevant states for Blocks-Words.

--- centroids-minimun_covering/script-extract-states.py
+++ centroids-minimun_covering/script-extract-states.py
@@ -17,25 +17,32 @@ def main():
             
             domain_file = domain_problem_path + '/domain.pddl'
             template_file = domain_problem_path + '/p' + pb + '.pddl'
-            goals_file = domain_problem_path + '/goals.txt'
-            # goals_file = domain_problem_path + '/goals-no_real_hyp.txt'
+            # goals_file = domain_problem_path + '/goals.txt'
+            goals_file = domain_problem_path + '/goals-no_real_hyp.txt'
 
             print ('$> ' + domain_problem_path)
 
-            cmd = 'python goal-related-states.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s centroid'
+            # cmd = 'python goal-related-states.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s centroid'
             # cmd = 'python goal-related-states.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-centroid'
             # cmd = 'python goal-related-states.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s minimum-covering'
             # cmd = 'python goal-related-states.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering'
 
             # cmd = 'python goal-related-states-no_real_goal.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s centroid'
             # cmd = 'python goal-related-states-no_real_goal.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-centroid'
-            # cmd = 'python goal-related-states-no_real_goal -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s minimum-covering'
-            # cmd = 'python goal-related-states-no_real_goal -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering'
+            # cmd = 'python goal-related-states-no_real_goal.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s minimum-covering'
+            # cmd = 'python goal-related-states-no_real_goal.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering'
 
             # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s centroid -c CLOSEST'
+            # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s centroid -c FARTHEST'
+
             # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-centroid -c CLOSEST'
+            # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-centroid -c FARTHEST'
+
+            # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s minimum-covering -c CLOSEST'
             # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s minimum-covering -c FARTHEST'
-            # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering -c FARTHEST'
+            
+            # cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering -c CLOSEST'
+            cmd = 'python goal-related-states-deception.py -d ' + domain_file + ' -p ' + template_file + ' -g ' + goals_file + ' -s r-minimum-covering -c FARTHEST'
             
             print(cmd)
             os.system(cmd)
